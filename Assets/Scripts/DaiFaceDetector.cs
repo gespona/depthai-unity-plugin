f8fde3a7acc19941aaadb17ec2e3366da02032d6
Gerard Espona <gespona@yahoo.es>
2021-04-21 03:37:38 +0200

    Face detector example

---
+++ Assets/Scripts/DaiFaceDetector.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class DaiFaceDetector : MonoBehaviour
+{
+    //Lets make our calls from the Plugin
+
+    [DllImport("libdepthai-core", CallingConvention = CallingConvention.Cdecl)]
+    private static extern bool InitFaceDetector(string nnPath);
+
+    [DllImport("libdepthai-core", CallingConvention = CallingConvention.Cdecl)]
+    private static extern bool FaceDetectorPreview(IntPtr data, int width, int height);
+
+    [DllImport("libdepthai-core", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int FaceDetectorResultsMX();
+
+    [DllImport("libdepthai-core", CallingConvention = CallingConvention.Cdecl)]
+    private static extern int FaceDetectorResultsMY();
+
+    [DllImport("libdepthai-core", CallingConvention = CallingConvention.Cdecl)]
+    private static extern bool FinishFaceDetector();
+
+
+    public Image cameraImage;
+    private Texture2D cameraTexture;
+    
+    private Texture2D tex;
+    private Color32[] pixel32;
+    private GCHandle pixelHandle;
+    private IntPtr pixelPtr;
+
+    private bool deviceRunning;
+    
+    public int faceDetectorResultsMX;
+    public int faceDetectorResultsMY;
+
+    public Text valueCenterX;
+    public Text valueCenterY;
+
+    void Start()
+    {
+        cameraTexture = new Texture2D(300, 300);    
+        InitTexture();
+        cameraImage.material.mainTexture = tex;
+        deviceRunning = false;
+    }
+
+    public void ConnectDevice()
+    {
+        InitFaceDetector(Application.dataPath+"/Models/face-detection-retail-0004_openvino_2021.2_4shave.blob");
+        deviceRunning = true;
+    }
+
+    public void FinishDevice()
+    {
+        if (deviceRunning) 
+        {
+            deviceRunning = false;
+            FinishFaceDetector();
+        }
+    }
+
+    void InitTexture()
+    {
+        tex = new Texture2D(300, 300, TextureFormat.ARGB32, false);
+        pixel32 = tex.GetPixels32();
+        //Pin pixel32 array
+        pixelHandle = GCHandle.Alloc(pixel32, GCHandleType.Pinned);
+        //Get the pinned address
+        pixelPtr = pixelHandle.AddrOfPinnedObject();
+    }
+
+    void OnApplicationQuit()
+    {
+        FinishDevice();
+        //Free handle
+        pixelHandle.Free();
+    }
+
+    void Update()
+    {
+        if (deviceRunning)
+        {
+            FaceDetectorPreview(pixelPtr, tex.width, tex.height);
+            faceDetectorResultsMX = FaceDetectorResultsMX();
+            faceDetectorResultsMY = FaceDetectorResultsMY();
+            valueCenterX.text = faceDetectorResultsMX.ToString();
+            valueCenterY.text = faceDetectorResultsMY.ToString();
+
+            tex.SetPixels32(pixel32);
+            tex.Apply();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # depthai-unity-plugin
-DepthAI Unity Plugin
+
+![Demo](img/depthai-unity-plugin-face-detector.gif)
+DepthAI Unity Plugin is native plugin designed to bring DepthAI cameras (OAK-1, OAK-D cameras) to Unity
+
+OAK cameras are Edge AI cameras powered by Intel Myriad-X Inference processor, so cameras are able to inference deep learning models.
+
+More info: https://www.luxonis.com
+
+DepthAI Unity Plugin is based on C++ DepthAI-Core library
+
+# Main Goals
+
+- Provide predefined pipelines (MobileNet, Face Detector, Pose, Face Mesh, Eye gaze, Hand Tracking, ....) very easy to use for Unity users (non-developers) p.eg: Face Detector prefab just drag and drop to your scene
+
+- Provide full "low" API to create gen2 pipelines inside Unity
+
+- Provide virtual OAK cameras to use inside Unity (p.eg: syntetic dataset creaation)
+
+# Usage
+
+## Face Detector Example
+
+In new scene, just drag and drop the FaceDetector prefab
+
+You can check example scene: FaceDetector
+
+Example using face detector results to control airplane in demo scene from OverCloud asset/package (not included in this repo)
+
+# Roadmap
+
+- Add more predefined gen2 pipelines: 3D depth information, Emotion Detector, MobileNet, Face Mesh, Human Body Pose Estimation, Eye gaze, ....
+- Multiple device support
+- Virtual OAK cameras
+- Plugin version for Windows and Linux
+- Low level API to create pipelines from Unity
+- Support to create pipelines using node-based tools like Playmaker, Bolt, ..
+- Advanced examples like the airplane:
+  - Motion Capture using human pose estimation
+  - Face animation and lip sync using face mesh
+  - Parallax camera movement using face pose
+
+Please star / watch this repo and stay tunned !
+
+# Requirements
+
+Tested on
+
+Unity 2020.1.7f (macOS Catalina 10.15.4)
+More coming soon !
+
+All render pipelines supported :)
+
+# Preview Version
+
+Work in this repo is at very early stage
+
+Until July 2021 we'll be very busy working to complete and submit our proposal for OpenCV Spatial AI Contest so probably we'll not have enough time to update this repo fast enough. That said the plugin had a very warm welcome and rised lot of interest so we commit to work on this plugin and deliver the roadmap asap.
